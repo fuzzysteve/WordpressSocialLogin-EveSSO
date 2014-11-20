d97bc43ed02563e6905eb3acae27063b376c9555
Steve Anderson / Steve Ronuken <steve@fuzzysteve.co.uk>
2014-11-20 04:17:05 +0000

    Create wsl.providers.php

---
+++ wsl.providers.php
@@ -0,0 +1,16 @@
+add
+
+         ARRAY(
+                 "provider_id"       => "EveSSO",
+                 "provider_name"     => "EveSSO",
+                 "callback"          => true,
+                 "new_app_link"      => "https://developers.eveonline.com",
+                 "require_client_id" => true,
+                 "default_network"  => false,
+                 "cat"               => "misc",
+         ),
+
+
+
+
+to the file wsl.providers.php in includes/settings for the plugin
